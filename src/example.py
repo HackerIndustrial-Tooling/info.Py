5b9035c71416c40146a16676a5b932701184b090
Monero-HackerIndustrial <106575456+Monero-HackerIndustrial@users.noreply.github.com>
2022-06-21 01:41:00 -0500

    added class for info

---
+++ src/example.py
@@ -0,0 +1,5 @@
+from info import Info
+
+info = Info()
+
+print(info.platform)
--- src/info.py
+++ src/info.py
@@ -1,6 +1,22 @@
 import platform
-
+import os
+import getpass
 
 class Info:
     def __init__(self):
         self.platform = platform.system()
+        if self.platform == "Linux" or self.platform == "Darwin":
+            self.posixSystem()
+    def posixSystem(self):
+        self.home = os.environ['HOME']
+        self.user = getpass.getuser()
+        self.release = platform.release()
+        self.processor = platform.processor()
+    def windows(self):
+        return True
+    def osx(self):
+        # osx specic stuff
+        return True
+    def linux(_self):
+        # linux specific stuff
+        return True
