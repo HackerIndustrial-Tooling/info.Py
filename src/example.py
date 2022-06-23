1dd8b7d5ce4f95766459157f587a3a7126c4a05e
Monero-HackerIndustrial <106575456+Monero-HackerIndustrial@users.noreply.github.com>
2022-06-22 20:30:24 -0500

    enumerating process list

--- src/example.py
+++ src/example.py
@@ -3,3 +3,5 @@ from info import Info
 info = Info()
 
 print(info.platform)
+#get all the running proccesses
+info.processList
--- src/info.py
+++ src/info.py
@@ -5,8 +5,22 @@ import getpass
 class Info:
     def __init__(self):
         self.platform = platform.system()
+        self.processList = {}
         if self.platform == "Linux" or self.platform == "Darwin":
             self.posixSystem()
+        if self.platform == "Linux":
+            self.linux()
+    def getProcess(self):
+        pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+        processList = []
+        for pid in pids:
+            _proInfo = {}
+            pro = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
+            pro = pro.decode("utf-8").split("\x00")[0]
+            _proInfo["pid"] = pid
+            _proInfo["process"] = pro
+            processList.append(_proInfo)
+        return processList
     def posixSystem(self):
         self.home = os.environ['HOME']
         self.user = getpass.getuser()
@@ -17,6 +31,7 @@ class Info:
     def osx(self):
         # osx specic stuff
         return True
-    def linux(_self):
+    def linux(self):
+        self.processList = self.getProcess()
         # linux specific stuff
         return True
